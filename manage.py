29cfe61c3bde199735016e23982bac080cc2a827
Richardbmk <rdobmk@gmail.com>
2022-12-26 14:08:08 +0100

    RESTful Routes

--- manage.py
+++ manage.py
@@ -17,5 +17,11 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@cli.command('seed_db')
+def seed_db():
+  db.session.add(User(username='michael', email='hermanmu@gmail.com'))
+  db.session.add(User(username='max', email='max@gmail.com'))
+  db.session.commit()
+
 if __name__ == '__main__':
   cli()
